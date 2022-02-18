afed55afc6d58c4719147b415c974f2b8fd735a5
simrancc <simrancc@umich.edu>
2022-02-18 16:20:37 -0500

    test students

--- src/component/testimonial/Testimonial.js
+++ src/component/testimonial/Testimonial.js
@@ -251,7 +251,7 @@ const studentTestimonials = [
         image: "images/testimonials/etwitmyer.jpeg",
     },
     {
-        name: "Lucy",
+        name: "University student, Minnesota",
         testimony:
             "I appreciated the opportunity to continue learning during my winter break, interact with diverse students, and engage in material about a topic I would not have the chance to learn about otherwise.",
         image: "images/testimonials/lucy.jpg",
@@ -263,7 +263,7 @@ const studentTestimonials = [
         image: "images/testimonials/michelle.jpg",
     },
     {
-        name: "Faraz",
+        name: "12th grader, Australia",
         testimony:
             "This program has given me a thorough introductionary understanding of quantum computing and mainly how interesting and fun it is. If the correct opportunity arises in the future, I would be more than happy to join another course/study about quantum computing agian. It would not only be a missed opportunity if I didn't further my knowledge of quantum computing, but also a missed opportunity to enjoy a very fun subject.",
         image: "images/testimonials/faraz.JPG",
