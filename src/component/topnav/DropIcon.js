282d7007bdd814cd2d2803b26c5dfa2ba5c961f5
kumailp <kumailpirzada@gmail.com>
2022-02-21 00:05:12 +0500

    Responsive issues and dropdown nav

---
+++ src/component/topnav/DropIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DropIcon = () => {
+  return (
+    <svg
+      class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch"
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      data-testid="KeyboardArrowDownIcon"
+      width="24"
+      height="24"
+    >
+      <path
+        d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"
+        fill="black"
+      ></path>
+    </svg>
+  );
+};
+
+export default DropIcon;
--- src/component/topnav/Topnav.js
+++ src/component/topnav/Topnav.js
@@ -2,15 +2,17 @@ import "./Topnav.scss";
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import DropIcon from "./DropIcon";
 
 function Topnav() {
   const [nav, setNav] = useState(false);
+  const [show, setShow] = useState(false);
   const toggleNav = () => setNav(!nav);
   const topLogo = {
     width: "100px",
-    marginLeft: "-50%",
+    // marginLeft: "-50%",
   };
-
+  console.log(show);
   const Items = [
     {
       label: "Home",
@@ -29,6 +31,15 @@ function Topnav() {
       external: true,
     },
     {
+      label: "Donate",
+      link: "https://codeconnects.org/donate.html",
+      active: false,
+      external: true,
+    },
+  ];
+
+  const dropItems = [
+    {
       label: "Qubit by Qubit",
       link: "https://www.qubitbyqubit.org",
       active: false,
@@ -43,12 +54,6 @@ function Topnav() {
       label: "Get involved",
       link: "/involved",
       active: false,
-    },
-    {
-      label: "Donate",
-      link: "https://codeconnects.org/donate.html",
-      active: false,
-      external: true,
     },
   ];
 
@@ -74,7 +79,7 @@ function Topnav() {
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div className={`navbar-collapse collapse ${nav && "show"}`}>
+        <div className={`navbar-collapse collapse nav-con ${nav && "show"}`}>
           <ul className="nav navbar-nav ml-auto mr-65">
             {Items.map((item, idx) => (
               <Element
@@ -84,17 +89,32 @@ function Topnav() {
                 external={item.external}
               />
             ))}
+            <div className={`nav-item px-8 pr-16 text-uppercase`}>
+              <li style={{ cursor: "pointer", marginTop: 6 }} onClick={() => setShow(!show)}>
+                Programs <DropIcon />
+              </li>
+              <div className="dropDown">
+                {show &&
+                  dropItems.map((item, idx) => (
+                    <Element
+                      label={item.label}
+                      active={item.active}
+                      link={item.link}
+                      external={item.external}
+                    />
+                  ))}
+              </div>
+            </div>
+            <a
+              className={`btn register-btn d-inline-block `}
+              href="https://codeconnects.org/register.html"
+              style={{ marginRight: "-100px" }}
+              target="_blank"
+            >
+              Register
+            </a>
           </ul>
         </div>
-
-        <a
-          className={`btn register-btn d-inline-block `}
-          href="https://codeconnects.org/register.html"
-          style={{ marginRight: "-100px" }}
-          target="_blank"
-        >
-          Register
-        </a>
       </div>
     </nav>
   );
