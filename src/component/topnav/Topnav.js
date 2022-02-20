55fd5c9d28a24570dc7c2986f19d399554403741
kumailp <kumailpirzada@gmail.com>
2022-02-21 00:37:41 +0500

    topnav handle click away issues

--- src/component/topnav/Topnav.js
+++ src/component/topnav/Topnav.js
@@ -1,7 +1,7 @@
 import "./Topnav.scss";
-
+import ClickAwayListener from 'react-click-away-listener';
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import DropIcon from "./DropIcon";
 
 function Topnav() {
@@ -12,7 +12,14 @@ function Topnav() {
     width: "100px",
     // marginLeft: "-50%",
   };
-  console.log(show);
+  // console.log(show);
+  const location = useLocation()
+  console.log("LOC: ", location)
+
+  React.useEffect(() => {
+    setShow(false);
+  }, [location?.pathname])
+
   const Items = [
     {
       label: "Home",
@@ -25,16 +32,15 @@ function Topnav() {
       active: false,
     },
     {
-      label: "CodeConnects",
-      link: "https://codeconnects.org",
-      active: false,
-      external: true,
-    },
-    {
       label: "Donate",
       link: "https://codeconnects.org/donate.html",
       active: false,
       external: true,
+    },
+    {
+      label: "Get involved",
+      link: "/involved",
+      active: false,
     },
   ];
 
@@ -46,77 +52,80 @@ function Topnav() {
       external: true,
     },
     {
-      label: "Train",
-      link: "/train",
+      label: "CodeConnects",
+      link: "https://codeconnects.org",
       active: false,
+      external: true,
     },
     {
-      label: "Get involved",
-      link: "/involved",
+      label: "Train",
+      link: "/train",
       active: false,
     },
   ];
 
   return (
-    <nav id="Topnav" className="navbar navbar-expand-lg navbar-light">
-      <div className="container">
-        <Link class="navbar-brand" to="/">
-          <img
-            src={
-              "/images/TCS_logo-1 copy.png" || "https://via.placeholder.com/80"
-            }
-            style={topLogo}
-            className="rounded-circle"
-          />
-        </Link>
-        <button
-          onClick={toggleNav}
-          className={`btn d-inline-block d-lg-none ml-auto ${
-            !nav && "collapsed"
-          }`}
-          type="button"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div className={`navbar-collapse collapse nav-con ${nav && "show"}`}>
-          <ul className="nav navbar-nav ml-auto mr-65">
-            {Items.map((item, idx) => (
-              <Element
-                label={item.label}
-                active={item.active}
-                link={item.link}
-                external={item.external}
-              />
-            ))}
-            <div className={`nav-item px-8 pr-16 text-uppercase`}>
-              <li style={{ cursor: "pointer", marginTop: 6 }} onClick={() => setShow(!show)}>
-                Programs <DropIcon />
-              </li>
-              <div className="dropDown">
-                {show &&
-                  dropItems.map((item, idx) => (
-                    <Element
-                      label={item.label}
-                      active={item.active}
-                      link={item.link}
-                      external={item.external}
-                    />
-                  ))}
+    <ClickAwayListener onClickAway={() => setShow(false)}>
+      <nav id="Topnav" className="navbar navbar-expand-lg navbar-light">
+        <div className="container">
+          <Link class="navbar-brand" to="/">
+            <img
+              src={
+                "/images/TCS_logo-1 copy.png" || "https://via.placeholder.com/80"
+              }
+              style={topLogo}
+              className="rounded-circle"
+            />
+          </Link>
+          <button
+            onClick={toggleNav}
+            className={`btn d-inline-block d-lg-none ml-auto ${!nav && "collapsed"
+              }`}
+            type="button"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div className={`navbar-collapse collapse nav-con ${nav && "show"}`}>
+            <ul className="nav navbar-nav ml-auto mr-65">
+              {Items.map((item, idx) => (
+                <Element
+                  label={item.label}
+                  active={item.active}
+                  link={item.link}
+                  external={item.external}
+                />
+              ))}
+              <div className={`nav-item px-8 pr-16 text-uppercase`}>
+                <li style={{ cursor: "pointer", marginTop: 6 }} onClick={() => setShow(!show)}>
+                  Programs <DropIcon />
+                </li>
+                <div className="dropDown">
+                  {show &&
+                    dropItems.map((item, idx) => (
+                      <Element
+                        label={item.label}
+                        active={item.active}
+                        link={item.link}
+                        external={item.external}
+                      />
+                    ))}
+                </div>
               </div>
-            </div>
-            <a
-              className={`btn register-btn d-inline-block `}
-              href="https://codeconnects.org/register.html"
-              style={{ marginRight: "-100px" }}
-              target="_blank"
-            >
-              Register
-            </a>
-          </ul>
+              <a
+                className={`btn register-btn d-inline-block `}
+                href="https://codeconnects.org/register.html"
+                style={{ marginRight: "-100px" }}
+                target="_blank"
+              >
+                Register
+              </a>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </ClickAwayListener>
   );
 }
 
